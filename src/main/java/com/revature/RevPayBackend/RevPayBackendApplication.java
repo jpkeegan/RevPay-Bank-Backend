b9d356506414a7f04bd531e4ce042ed7a490934d
jpkeegan <82060340+jpkeegan@users.noreply.github.com>
2023-03-28 14:12:34 -0400

    Initial Commit

---
+++ src/main/java/com/revature/RevPayBackend/RevPayBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.RevPayBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevPayBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevPayBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/revature/RevPayBackend/RevPayBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.RevPayBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevPayBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
