7468cfd399d74441f988e0f97b7ccc590da87fcc
Bridget Galang <galanbri@gmail.com>
2023-03-28 14:38:26 -0700

    added exceptions and logging to bank account

--- src/main/java/com/revature/RevPayBackend/entity/BankAccount.java
+++ src/main/java/com/revature/RevPayBackend/entity/BankAccount.java
@@ -2,6 +2,7 @@ package com.revature.RevPayBackend.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -9,6 +10,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Data
 @Entity
 @Table(name = "BankAccount")
