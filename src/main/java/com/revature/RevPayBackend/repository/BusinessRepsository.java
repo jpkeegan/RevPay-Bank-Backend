f1c219558926b8a97390dd5ba2a0604f7efbd497
Erich-D <33156265+Erich-D@users.noreply.github.com>
2023-03-28 13:05:45 -0700

    add business entity

---
+++ src/main/java/com/revature/RevPayBackend/repository/BusinessRepsository.java
@@ -0,0 +1,9 @@
+package com.revature.RevPayBackend.repository;
+
+import com.revature.RevPayBackend.entity.Business;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusinessRepsository  extends JpaRepository<Business,Long> {
+}
---
+++ src/main/java/com/revature/RevPayBackend/service/BusinessService.java
@@ -0,0 +1,13 @@
+package com.revature.RevPayBackend.service;
+
+import com.revature.RevPayBackend.entity.Business;
+
+import java.util.List;
+
+public interface BusinessService {
+    Business insert(Business business);
+    Business getById(Long id);
+    List<Business> getAll();
+    Business update(Business business);
+    boolean delete(Long id);
+}
