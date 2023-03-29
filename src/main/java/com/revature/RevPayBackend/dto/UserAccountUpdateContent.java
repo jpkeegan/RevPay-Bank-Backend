b71f338f06868d40493b06a82f89504e1894ee32
edduclervils <edduclervils@gmail.com>
2023-03-29 16:49:57 -0400

    Integrated Exceptions to User Account Table. Changed logging (Insertion displayed Password) Corrected Update functionality for User Account

---
+++ src/main/java/com/revature/RevPayBackend/dto/UserAccountUpdateContent.java
@@ -0,0 +1,16 @@
+package com.revature.RevPayBackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data@NoArgsConstructor@AllArgsConstructor
+public class UserAccountUpdateContent {
+    public String username;
+    public String password;
+    public String email;
+    public Long phoneNumber;
+    public String name;
+    public String address;
+    public boolean isBusinessAccount;
+}
